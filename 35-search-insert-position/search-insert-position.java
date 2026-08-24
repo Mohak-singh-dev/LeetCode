4da4cc50e5a2f594c32d7c029fb07d3a861bd815
Mohak Singh <mohak_singh@outlook.com>
2026-08-24 13:11:55 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (48.66%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int bin(int[] nums,int low,int high,int key){
+        int mid = 1;
+        while(low<=high){
+            mid = low +(high-low)/2;
+            if (nums[mid]==key){
+                return mid;
+            }
+            else if (nums[mid]>key){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return mid;
+    }
+    public int searchInsert(int[] nums, int target) {
+        int result = bin(nums,0,nums.length-1,target);
+        if (nums[result]<target){
+            return result+1;
+        }
+        return result;
+    }
+}
